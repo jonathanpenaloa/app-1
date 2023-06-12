ee68ef3e70d35c355e4d721efa89d70228fd1c5a
Jonathan Pena-Loa <jpena-loa@LABA.PSWAS.LOCAL>
2023-06-12 13:52:17 -0700

    added text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jonathan Pena-Loa just made a commit 
         </a>
       </header>
     </div>
